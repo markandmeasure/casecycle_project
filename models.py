0adef458b8e57755225fadafbbf24af1dd0a5189
markandmeasure <petterianssen@gmail.com>
2025-08-03 14:42:10 +0200

    Add opportunity model and API

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Float
 
 from database import Base
 
@@ -14,4 +14,9 @@ class Opportunity(Base):
     __tablename__ = "opportunities"
 
     id = Column(Integer, primary_key=True, index=True)
-    name = Column(String, unique=True, index=True, nullable=False)
+    title = Column(String, index=True, nullable=False)
+    market_description = Column(String, nullable=True)
+    tam_estimate = Column(Float, nullable=True)
+    growth_rate = Column(Float, nullable=True)
+    consumer_insight = Column(String, nullable=True)
+    hypothesis = Column(String, nullable=True)
